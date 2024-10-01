15707d49f6822643458e52999f9dcf31cd08c675
mtsaurus <qksrurwnsql@naver.com>
2024-10-01 12:59:07 +0900

    first commit

---
+++ src/main/java/uoscs/capstone/allyojo/AllyojoApplication.java
@@ -0,0 +1,13 @@
+package uoscs.capstone.allyojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllyojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllyojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uoscs/capstone/allyojo/AllyojoApplicationTests.java
@@ -0,0 +1,13 @@
+package uoscs.capstone.allyojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllyojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
